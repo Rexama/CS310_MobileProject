cb7f6d55c794b02259d09131f28cd85a2efd6e3b
Rekin Barışıcı <57857241+Rexama@users.noreply.github.com>
2021-11-24 10:56:24 +0300

    week 6 final folder uploaded

---
+++ week6_starter/lib/utils/api.dart
@@ -0,0 +1,7 @@
+class API {
+  static const allPosts = 'https://jsonplaceholder.typicode.com/posts';
+  static const singlePost = 'https://jsonplaceholder.typicode.com/posts/';
+
+  static const allUsers = 'https://jsonplaceholder.typicode.com/users';
+  static const singleUser = 'https://jsonplaceholder.typicode.com/users/';
+}
