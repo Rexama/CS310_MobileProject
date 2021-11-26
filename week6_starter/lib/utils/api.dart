9284287fddba172c6b70981227ebad601eae2970
Rekin Barışıcı <57857241+Rexama@users.noreply.github.com>
2021-11-26 13:44:36 +0300

    reset

--- week6_starter/lib/utils/api.dart
+++
@@ -1,7 +0,0 @@
-class API {
-  static const allPosts = 'https://jsonplaceholder.typicode.com/posts';
-  static const singlePost = 'https://jsonplaceholder.typicode.com/posts/';
-
-  static const allUsers = 'https://jsonplaceholder.typicode.com/users';
-  static const singleUser = 'https://jsonplaceholder.typicode.com/users/';
-}
