a06b6b6e909e88bf51d30f6372ce5167027b0bb6
Tahir Turgut <tahirturgut@sabanciuniv.edu>
2021-12-27 16:02:12 +0300

    implementing FutureBuilder in feeds, sync database instantaneously

--- week6_starter/lib/routes/newsView.dart
+++ week6_starter/lib/routes/newsView.dart
@@ -1,23 +1,13 @@
 import 'dart:async';
+import 'dart:io';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:week6_starter/models/Comment.dart';
 import 'package:week6_starter/models/News.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:week6_starter/models/Blog.dart';
-import 'package:week6_starter/models/Users.dart';
-import 'package:week6_starter/services/auth.dart';
 import 'package:week6_starter/services/db.dart';
 import 'package:week6_starter/utils/color.dart';
-import 'package:week6_starter/utils/dimension.dart';
-import 'package:week6_starter/utils/styles.dart';
-import 'package:intl/intl.dart';
-import 'package:comment_box/comment/comment.dart';
-import 'package:comment_box/comment/test.dart';
-import 'package:comment_box/main.dart';
 
 class NewsView extends StatefulWidget {
   @override
@@ -38,133 +28,143 @@ class NewsView extends StatefulWidget {
 class _NewsView extends State<NewsView> {
   List<Comment> comments = [];
   DBService db = DBService();
-  late Timer _timer;
+  late Future _future = db.getComments(comments, widget.content.newsId, false);
   String comment = "";
 
-  /*@override
+  @override
   void initState() {
-    db.getComments(comments, widget.content.newsId, false).then((data) {
-      setState(() {
-        this.comments = comments;
-      });
-    });
-  }*/
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    print(comments.length);
-    print("aa");
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColors.darkBlue,
-      ),
-      body: ListView(
-        padding: const EdgeInsets.all(8.0),
-        children: [
-          SizedBox(height: 15),
-          Center(
-            child: Text(
-              widget.content.title,
-              style: GoogleFonts.robotoSlab(
-                color: AppColors.darkestBlue,
-                fontWeight: FontWeight.w800,
-                fontSize: 24,
-              ),
+    return FutureBuilder(
+        future: _future,
+        builder: (context, snapshot) {
+          _future.then((data) {
+            setState(() {
+              this.comments = comments;
+            });
+          });
+          if (comments.isEmpty) {
+            sleep(Duration(seconds: 1));
+            //setState(() {});
+          }
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: AppColors.darkBlue,
             ),
-          ),
-          SizedBox(height: 15),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 200.0,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  //let's add the height
-                  image: DecorationImage(
-                      image: NetworkImage(widget.content.image.toString()),
-                      fit: BoxFit.cover),
-                  border: Border.all(
-                    color: AppColors.midBlue,
-                    width: 3,
-                  ),
-                  borderRadius: BorderRadius.circular(5),
-                ),
-              ),
-              SizedBox(
-                height: 15.0,
-              ),
-              Container(
-                padding: EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  border: Border.all(color: AppColors.midBlue),
-                  color: AppColors.whiteBlue,
-                ),
-                child: Text(
-                  widget.content.subtitle,
-                  style: GoogleFonts.robotoSlab(
-                    color: AppColors.darkestBlue,
-                    fontWeight: FontWeight.w800,
-                    fontSize: 17,
+            body: ListView(
+              padding: const EdgeInsets.all(8.0),
+              children: [
+                SizedBox(height: 15),
+                Center(
+                  child: Text(
+                    widget.content.title,
+                    style: GoogleFonts.robotoSlab(
+                      color: AppColors.darkestBlue,
+                      fontWeight: FontWeight.w800,
+                      fontSize: 24,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 15.0,
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  widget.content.content,
-                  style: GoogleFonts.robotoSlab(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16.0,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 15.0,
-              ),
-              ListTile(
-                  title: Text(
-                    "yürüyen uçak", //comments[index].username,
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  subtitle:
-                      Text("uzun adam çok yaşa" //comments[index].content),
-                          )),
-              SizedBox(
-                height: 15.0,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter your comment here ...',
-                  hintStyle: TextStyle(
-                    color: AppColors.openBlue,
-                  ),
-                ),
-                maxLines: 1,
-                onChanged: (value) {
-                  comment = value;
-                },
-              ),
-              const SizedBox(height: 8),
-              ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: AppColors.midBlue,
-                  ),
-                  child: Text("Comment"),
-                  onPressed: () {
-                    //db.addComment(
-                    //  comment, userClass.userName, "d327", false);
-                  }),
-            ],
-          )
-        ],
-      ),
-    );
+                SizedBox(height: 15),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 200.0,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        //let's add the height
+                        image: DecorationImage(
+                            image:
+                                NetworkImage(widget.content.image.toString()),
+                            fit: BoxFit.cover),
+                        border: Border.all(
+                          color: AppColors.midBlue,
+                          width: 3,
+                        ),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: AppColors.midBlue),
+                        color: AppColors.whiteBlue,
+                      ),
+                      child: Text(
+                        widget.content.subtitle,
+                        style: GoogleFonts.robotoSlab(
+                          color: AppColors.darkestBlue,
+                          fontWeight: FontWeight.w800,
+                          fontSize: 17,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        widget.content.content,
+                        style: GoogleFonts.robotoSlab(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    comments.isEmpty ? Container() : ListTile(
+                        title: Text(
+                          comments[0].username,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Text(comments[0].content, style: GoogleFonts.robotoSlab(
+                          color: AppColors.darkestBlue,
+                          fontSize: 15.0,
+                        ),),
+                    ),
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter your comment here ...',
+                        hintStyle: TextStyle(
+                          color: AppColors.openBlue,
+                        ),
+                      ),
+                      maxLines: 1,
+                      onChanged: (value) {
+                        comment = value;
+                      },
+                    ),
+                    const SizedBox(height: 8),
+                    ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: AppColors.midBlue,
+                        ),
+                        child: Text("Comment"),
+                        onPressed: () {
+                          //db.addComment(
+                          //  comment, userClass.userName, "d327", false);
+                        }),
+                  ],
+                )
+              ],
+            ),
+          );
+        });
   }
 }
 
